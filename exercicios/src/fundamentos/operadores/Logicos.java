3edd4fd24fbe9b8230577495f50b57af05613a9c
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-02-09 18:28:10 -0200

    Operadores Logicos

---
+++ exercicios/src/fundamentos/operadores/Logicos.java
@@ -0,0 +1,39 @@
+package fundamentos.operadores;
+
+public class Logicos {
+
+	public static void main(String[] args) {
+
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+
+		System.out.println(condicao1 && !condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!!condicao1);
+		System.out.println(!condicao2);
+
+		System.out.println("Table verdade E");
+		System.out.println(true && true);
+		System.out.println(true && false);
+//		System.out.println(false && true);
+//		System.out.println(false && false);
+
+		System.out.println("\nTable verdade OU (OR)");
+//		System.out.println(true || true);
+//		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+		System.out.println("\nTable verdade OU Exclusivo (XOR)");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+
+		System.out.println("\nTable verdade NOT");
+		System.out.println(!true);
+		System.out.println(!false);
+	}
+
+}
