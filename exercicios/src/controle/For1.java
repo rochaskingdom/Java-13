10a3daae7a4f168b29ab95c8218f77691edba60d
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-11 11:34:41 -0200

    FOR 01

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+
+		for (int contador = 1; contador <= 10; contador++) {
+			System.out.printf("i = %d\n", contador);
+		}
+
+//		for (;;) {
+//			System.out.println("Infinito!");
+//		}
+
+	}
+
+}
