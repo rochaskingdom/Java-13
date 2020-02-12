97cca185d78dd4c962bd2edfe5bf5172178727f4
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-12 14:39:04 -0200

    Desafio Método

--- exercicios/src/classe/DataTest.java
+++ exercicios/src/classe/DataTest.java
@@ -14,8 +14,8 @@ public class DataTest {
 		d2.mes = 02;
 		d2.ano = 2020;
 
-		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
 
 	}
 
