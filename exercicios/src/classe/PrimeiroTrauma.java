234ce59cb71af2d3153b7447c47b4fd018dcc31d
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-02-13 12:48:38 -0200

    Desafio Primeiro Trauma

---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class PrimeiroTrauma {
+
+	int a = 3; // não pode mexer aqui!
+
+	public static void main(String[] args) {
+		// pode mexer apenas no main
+
+		System.out.println(a);
+	}
+}
