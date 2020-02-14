25bf9932267a19cb8cf12329f05e21f74057ef54
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-02-14 17:18:21 -0200

    Equals na Pratica

---
+++ exercicios/src/classe/Equals.java
@@ -0,0 +1,24 @@
+package classe;
+
+import java.util.Date;
+
+public class Equals {
+
+	public static void main(String[] args) {
+
+		Usuario u1 = new Usuario();
+		u1.nome = "Vinicius Rocha";
+		u1.email = "viniciusr.rocha@outlook.com";
+
+		Usuario u2 = new Usuario();
+		u2.nome = "Vinicius Rocha";
+		u2.email = "viniciusr.rocha@outlook.com";
+
+		System.out.println(u1 == u2);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+
+		System.out.println(u2.equals(new Date()));
+	}
+
+}
