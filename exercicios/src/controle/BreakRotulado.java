d83db5bd88414941d0d563e7fb1bac23b5419e8c
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-02-14 00:42:28 -0200

    Desafio Array

--- exercicios/src/controle/BreakRotulado.java
+++ exercicios/src/controle/BreakRotulado.java
@@ -3,9 +3,8 @@ package controle;
 public class BreakRotulado {
 
 	public static void main(String[] args) {
-//		nao utilizar essa forma
-		externo: for (int i = 0; i < 3; i++) {
-			interno: for (int j = 0; j < 3; j++) {
+for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				if (i == 1) {
 					break;
 				}
