fb14b179d77c7f2ee1792d2f675ced8d4d635e15
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-02-15 01:17:15 -0200

    Set 02

---
+++ exercicios/src/colecoes/ConjuntoComportado.java
@@ -0,0 +1,31 @@
+package colecoes;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+
+	public static void main(String[] args) {
+
+//		Set<String> listaAprovados = new HashSet<>();
+		SortedSet<String> listaAprovados = new TreeSet<>();
+		listaAprovados.add("Ana");
+		listaAprovados.add("Carlos");
+		listaAprovados.add("Luca");
+		listaAprovados.add("Pedro");
+
+		for (String candidato : listaAprovados) {
+			System.out.println(candidato);
+		}
+
+		SortedSet<Integer> nums = new TreeSet<>();
+		nums.add(1);
+		nums.add(2);
+		nums.add(120);
+		nums.add(6);
+
+		for (int n : nums) {
+			System.out.println(n);
+		}
+	}
+}
